9cc5659740f4be84e280305ab1178acaa40cd6f7
Alethia Quintero <alethiaq22@gmail.com>
2019-07-29 21:58:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,3 +7,19 @@ class Author
 
   def posts
     Posts.all.select {|post| post.author == self}
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+
+    post.author = self
+  end
+
+  def post_count
+    Song.all.count {|post| post}
+  end
+end
