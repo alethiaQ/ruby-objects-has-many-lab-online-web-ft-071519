bd7924e1db3b74ef168b7732fe54315dd2a8c193
Alethia Quintero <alethiaq22@gmail.com>
2019-07-29 21:43:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
-class Song 
+class Song
   attr_accessor :artist, :name
   @@all = []
   def initialize(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all << self
   end
 
-  def artist_name 
-    song.artist = self 
-  end 
-end 
+  def artist_name
+    song.artist = self
+  end
+end
